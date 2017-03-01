b45969f45a6a47982d0fb54725d16b83db6e4e64
drybone <jackie_chan1994@hotmail.com>
2017-03-01 14:59:11 +0100

    Datum och tid

---
+++ public/js/setDate.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Leon on 2017-02-28.
+ */
+
+
+
+$(function() {
+    $("#startDate").datepicker();
+    $("#endDate").datepicker();
+});
---
+++ public/js/time.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Leon on 2017-02-27.
+ */
+
+$ function () {
+
+    $("#startTime").timepicker();
+        $("#endTime").timepicker();
+        timeOnly: true
+});
+
+
+
